64fee9f932f20757c859706ff753ef6bc53a6b12
sachin0986 <106465241+sachin0986@users.noreply.github.com>
2024-01-18 13:33:55 +0530

    added post API's

--- my-api.js
+++ my-api.js
@@ -1,7 +1,12 @@
 const express = require("express");
 //Database
 const database = require("./database");
+//All this for posting the data
+// Defination -----> 
+var bodyParser = require("body-parser");
 const shelf = express();
+shelf.use(bodyParser.urlencoded({extended: true}));  //In ordre to acess the post data we use that 
+shelf.use(bodyParser.json()); //this is just for making our code bug free
 
 //Get ------------------------------------------------------------------------------------------------------------>
 
@@ -64,7 +69,7 @@ shelf.get("/book/lang/:language", (request, response) => {
     const getBookbyLanguage = database.books.filter((book) => book.language.includes(request.params.language));
     if(getBookbyLanguage.length === 0)
     {
-        return response.json({error: `No book found for ${reqiest.params.language}`});
+        return response.json({error: `No book found for ${request.params.language}`});
     }
     return response.json({Book: getBookbyLanguage});
 });
@@ -97,6 +102,25 @@ shelf.get("/pub/:id", (request, response) => {
     }
     return response.json({Publication: getPublicationbyID});
 });
+
+// ------------------------------------------------------------------------------------------------------------>
+/*
+Route: "/books/:isbn"
+Description: API call to get specific publication in database
+Access: Public
+Parameters: isbn
+Methods: GET
+*/
+
+shelf.get("/pub/book/:isbn", (request, response) => {
+    const getPublicationByBook = database.publications.filter((publication) => publication.books.includes(request.params.isbn));
+    if(getPublicationByBook.length === 0)
+    {
+        return response.json({Error: `No Publication found for this Book ${request.params.isbn} isbn number`});
+    }
+    return response.json({Publication : getPublicationByBook});
+}) ;
+
 // ------------------------------------------------------------------------------------------------------------>
 /*
 Route: "/authors"
@@ -108,6 +132,7 @@ Methods: GET
 shelf.get("/authors", (request, response) => {
     return response.json({Authors: database.authors})
 });
+
 // ------------------------------------------------------------------------------------------------------------>
 /*
 Route: "/authors"
@@ -116,6 +141,7 @@ Access: Public
 Parameters: ID
 Methods: GET
 */
+
 shelf.get("/auth/:id", (request, response) => {
     const getAuthorbyID = database.authors.filter((author) => author.ID === parseInt(request.params.id));
     if(getAuthorbyID.length === 0)
@@ -125,7 +151,54 @@ shelf.get("/auth/:id", (request, response) => {
     return response.json({Author: getAuthorbyID});
 });
 
+// ------------------------------------------------------------------------------------------------------------>
+/*
+Route: "/authors"
+Description: API call to get specific authors in database
+Access: Public
+Parameters: ID
+Methods: GET
+*/
+
+shelf.get("/auth/book/:isbn", (request, response) => {
+    const getAuthorByBook = database.authors.filter((author) => author.books.includes(request.params.isbn));
+    if(getAuthorByBook.lenght === 0)
+    {
+        return response.json({Error : `No Author found for this Book ${request.params.isbn} number`});
+    }
+    return response.json({Author : getAuthorByBook});
+}) ;
 /*------------------------------------------------------------------------------------------------------------------- */
+
+
+
+//POST --------------------------------------------------------------------------------------------------------------
+/*
+Route: "/authors"
+Description: API call to get specific authors in database
+Access: Public
+Parameters: ID
+Methods: POST
+*/
+shelf.post("/book/new", (request, response) => {
+    const newBook = request.body;
+    database.books.push(newBook);
+    return response.json({UpdatedBook : database.books});
+});
+
+//POST --------------------------------------------------------------------------------------------------------------
+/*
+Route: "/authors"
+Description: API call to get specific authors in database
+Access: Public
+Parameters: ID
+Methods: POST
+*/
+shelf.post("/publication/new", (request, response) => {
+    const newPublication = request.body;
+    database.books.push(newPublication);
+    return response.json({UpdatedPublications: database.publications});
+});    
 
 shelf.listen(3000, () => {
     console.log("Server is up and running");
